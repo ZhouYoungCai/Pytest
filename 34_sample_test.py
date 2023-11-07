a961f27030bdd983073d64517ee0ca7bae8f19be
zhouyongcai <807923011@qq.com>
2023-11-07 20:54:41 +0800

    第5次修改

--- 34_sample_test.py
+++ 34_sample_test.py
@@ -3,4 +3,10 @@
 def inc(x):
     return x + 1
 def test_answer():
-    assert inc(4) == 5
+    assert inc(4) == 5  #assert代表断言
+class TestDemo:
+    def test_demo1(self):
+        pass
+
+    def test_demo2(self):
+        pass
